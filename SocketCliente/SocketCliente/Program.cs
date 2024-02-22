8f21e1ae2d40b4d642a8135645e7451c1392eca9
Rúben Vieira <110641459+RubenSousaVieira@users.noreply.github.com>
2024-02-22 11:04:07 +0000

    Add files via upload

--- SocketCliente/SocketCliente/Program.cs
+++ SocketCliente/SocketCliente/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 
 namespace SocketCliente
 {
@@ -23,17 +24,38 @@ namespace SocketCliente
             {
                 cliente.Connect(endPoint);
 
-                while(true)
+                // Imprimir a data e hora atual
+                Console.WriteLine("Data e Hora Atual: " + DateTime.Now);
+
+                // Imprimir o endereço IP do cliente
+                Console.WriteLine("Endereço IP do Cliente: " + ipAddress.ToString());
+
+                // Obter e imprimir o endereço MAC do cliente
+                NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+                String macAddress = String.Empty;
+                foreach (NetworkInterface adapter in nics)
+                {
+                    if (!adapter.GetPhysicalAddress().ToString().Equals(""))
+                    {
+                        macAddress = adapter.GetPhysicalAddress().ToString();
+                        break;
+                    }
+                }
+                Console.WriteLine("Endereço MAC do Cliente: " + macAddress);
+
+                while (true)
                 {
                     Console.WriteLine("Digite Mensagem: ");
                     string data = Console.ReadLine();
                     // vamos enviar em bytes os dados da nossa string data
                     cliente.Send(Encoding.UTF8.GetBytes(data));
                 }
-            } catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-            } finally
+            }
+            finally
             {
                 cliente.Close();
             }
